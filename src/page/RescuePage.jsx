9dd624c20729e70516ca39c4e75050aeeecbe7b0
heesane <tees3359@gmail.com>
2023-10-30 14:07:03 +0900

    fix : 멘트

--- src/page/RescuePage.jsx
+++ src/page/RescuePage.jsx
@@ -28,11 +28,11 @@ function Rescue (props){
                 status: 
                     response.data.robots_status === 0 ? "대기 중" :
                     response.data.robots_status === 1 ? "구조 중 - 해상 낙하 사고 발생" :
-                    response.data.robots_status === 2 ? "구조 중 - 피재자 거리 파악" :
+                    response.data.robots_status === 2 ? "구조 중 - 피재자 위치 파악" :
                     response.data.robots_status === 3 ? "구조 중 - 최단 거리 이동" :
                     response.data.robots_status === 4 ? "구조 중 - 튜브 전달" :
                     response.data.robots_status === 5 ? "구조 중 - 피재자 구조" :
-                    "구조 중 - 구조 완료" 
+                    "구조 완료" 
             };
             setRobotinfo(inputRobotinfo);
         })
@@ -55,17 +55,17 @@ function Rescue (props){
     
     return(
         <div>
-            {robotinfo.status === "대기 중" |  robotinfo.status === "복귀 중" ? <TopBar/> : <TopBarRed/>}
+            {robotinfo.status === "대기 중" |  robotinfo.status === "구조 완료" ? <TopBar/> : <TopBarRed/>}
             <h1 className="Map1Name">
             {
-                admin_port === 0 ? "부산" : "울산"
+                admin_port === 1 ? "울산" : "부산"
             }
             항</h1>
             <img className = 'Map1' src='https://github.com/SV-Summer-BootCamp-Team-F/.github/assets/113844225/8eb5a4bb-a652-49de-bf50-0f1a86c5ed36' alt="map1" />
             <h3 className="Map1info">Admin: {admin_name}</h3>
             <h3 className="Map1info">{robot_id}번 로봇</h3>
             {
-                robotinfo.status === "대기 중" | robotinfo.status === "복귀 중"? 
+                robotinfo.status === "대기 중" | robotinfo.status === "구조 완료"? 
                     <h3 className="Map1info">
                         현재상태: 
                         <span className="Map1info1">
